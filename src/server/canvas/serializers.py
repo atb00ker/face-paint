462072b9ebda5476472ea4ee3dbb6d5d3ba8a1ab
VallariAg <val.agl002@gmail.com>
2021-11-20 19:38:22 +0530

    [api] remove username from CanvasSerializer

--- src/server/canvas/serializers.py
+++ src/server/canvas/serializers.py
@@ -4,12 +4,10 @@ from rest_framework.serializers import ModelSerializer
 from .models import Canvas
 
 class CanvasSerializer(ModelSerializer):
-    username = serializers.CharField(max_length=128)
 
     class Meta:
         model = Canvas
         fields = (
             'id',
             'image_path',
-            'username',
         )
