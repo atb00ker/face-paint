0f6c8f24aa1736298fa2d1e9576d1da28750610a
Ajay Tripathi <ajay39in@gmail.com>
2021-11-25 20:38:20 +0530

    [deploy] Fixed deployment problems (After call)

--- deploy/init_container.sh
+++ deploy/init_container.sh
@@ -2,5 +2,5 @@
 
 python3 manage.py migrate
 # tail -f /dev/null
-python3 manage.py runserver 0.0.0.0:8000
-# uwsgi --ini uswgi.conf.ini --plugin python3
+# python3 manage.py runserver 0.0.0.0:8000
+uwsgi --ini uwsgi.conf.ini
